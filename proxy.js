8031a993e455f05f4b295fcb404e45dad4a547eb
Gabriel Timotei <gabtimotei@gmail.com>
2018-02-13 11:57:15 -0500

    Changed file to JS

---
+++ proxy.js
@@ -0,0 +1,32 @@
+const http = require('http')
+const url = require('url')
+
+console.log('Something')
+
+http.createServer(function (request, response) {
+  var proxy = http.request({
+    port: 80,
+    hostname: request.headers['host'],
+    method: request.method,
+    url: url.parse(request.url),
+    headers: request.headers
+  })
+
+  proxy.addListener('response', function (proxyResponse) {
+    proxyResponse.addListener('data', function (chunk) {
+      response.write(chunk, 'binary')
+    })
+    proxyResponse.addListener('end', function () {
+      response.end()
+    })
+    response.writeHead(proxyResponse.statusCode, proxyResponse.headers)
+  })
+
+  request.addListener('data', function (chunk) {
+    proxy.write(chunk, 'binary')
+  })
+
+  request.addListener('end', function () {
+    proxy.end()
+  })
+}).listen(8080)
